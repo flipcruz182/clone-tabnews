c40dd683dca82f9d3e32437fc609e1b3e4bd533d
flipcruz182 <167034258+flipcruz182@users.noreply.github.com>
2025-04-02 00:18:24 +0000

    oi, estou alterando minha homepage sem seguir o tutorial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1> Desafio: Tirar um sorriso de alguém com essa simples página das interwebs :) </h1>   
+    return <h1> Oi, meu nome é Bilu, e essa é minha primeira atividade como programador e usando o github! </h1>   
 }
 
 export default Home;
